a2dd09d78bc42e6ffcfcf0cb69458d5d9d8c5208
Lucas Rodrigues <lucas.bartllb@gmail.com>
2026-01-22 15:20:32 -0300

    criação middleware e rota de verificação via ID

---
+++ src/middlewares/chack-session-id.ts
@@ -0,0 +1,14 @@
+import type { FastifyReply, FastifyRequest } from "fastify"
+
+export async function checkSessionIdExist (req:FastifyRequest, res:FastifyReply) {
+
+     const  sessionID = req.cookies.sessionID 
+
+        if(!sessionID){
+            return res.status(401).send({
+                error:"Não autorizado"
+            })
+
+        }
+    
+}
--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,62 +1,118 @@
 import type { FastifyInstance } from 'fastify'
-import {z} from 'zod'
+import { z } from 'zod'
 import { db } from '../database.js'
 import { randomUUID } from 'node:crypto'
+import { checkSessionIdExist } from '../middlewares/chack-session-id.js'
 
 //Cookies ->Formas de manter contexto entre as requisições
 
 
-export async function transactionsRoutes(server:FastifyInstance) {
+export async function transactionsRoutes(server: FastifyInstance) {
+
+    /* POST */
 
     server.post('/', async (req, res) => {
-       //Queremos recuperar de body:{ title, amount, type: credit ou debit }
-       const createTransationBodySchema = z.object({
-        title: z.string(),
-        amount: z.number(),
-        type: z.enum(['credit' , 'debit'])
-       })
-       
-
-       //valida se as informaçoes e recebe os atributos de createTransationBodySchema
-       const {title, amount, type}  = createTransationBodySchema.parse(req.body)
-
-       // Procurando dentro dos cookies da requisiçao um sessionID 
-       let sessionID = req.cookies.sessionID 
-
-       if(!sessionID){
-
-        sessionID = randomUUID()
-
-        res.cookie('sessionID', sessionID,{
-            //Com o path definimos a rota que vai ter acesso aos cookies :
-            path:'/',
-            //Expiraçao dos cookies:
-            maxAge: 1000 * 60 * 60 * 24 * 7, // 7 dias
+        //Queremos recuperar de body:{ title, amount, type: credit ou debit }
+        const createTransationBodySchema = z.object({
+            title: z.string(),
+            amount: z.number(),
+            type: z.enum(['credit', 'debit'])
         })
 
-       }
 
-       await db('transactions').insert({
-        id:randomUUID(),
-        title,
-        amount: type === 'credit' ? amount : amount * -1,
-        session_id: sessionID
-       })
+        //valida se as informaçoes e recebe os atributos de createTransationBodySchema
+        const { title, amount, type } = createTransationBodySchema.parse(req.body)
 
-       return res.status(201).send('Enviado')
+        // Procurando dentro dos cookies da requisiçao um sessionID 
+        let sessionID = req.cookies.sessionID
+
+        if (!sessionID) {
+
+            sessionID = randomUUID()
+
+            res.cookie('sessionID', sessionID, {
+                //Com o path definimos a rota que vai ter acesso aos cookies :
+                path: '/',
+                //Expiraçao dos cookies:
+                maxAge: 1000 * 60 * 60 * 24 * 7, // 7 dias
+            })
+
+        }
+
+        await db('transactions').insert({
+            id: randomUUID(),
+            title,
+            amount: type === 'credit' ? amount : amount * -1,
+            session_id: sessionID
+        })
+
+        return res.status(201).send('Enviado')
     })
 
-    server.get('/',async()=>{
-        //Realiza a soma dos valores e com (as:'amount') muda o nome da coluna 
-        const transactions = await db('transactions').select('*')
-        return {transactions }
-    })
 
-    server.get('/summary',async()=>{
-        //Realiza a soma dos valores e com (as:'amount') muda o nome da coluna 
-        const summary = await db('transactions').sum('amount',{as: 'amount'})
-        return {summary}
-    })
+    /* GET DE LISTAGEM TODAS AS TRANSAÇÕES */
+
+    //O segundo parametro nos definimos que antes da handler(rota) executar, 
+    // irá passar primeiro pelo middleware (checkSessionIdExist) 
+
+    server.get('/',
+        { preHandler: [checkSessionIdExist] }
+        , async (req, res) => {
+
+
+            //Puxar transaçoes unicas || Valida se o cookie esta presente
+            const { sessionID } = req.cookies
+
+            //Realiza a soma dos valores e com (as:'amount') muda o nome da coluna 
+            const transactions = await db('transactions')
+                .where('session_id', sessionID)
+                .select('*')
+
+            return { transactions }
+        })
+
+
+    /* GET DE LISTAGEM POR ID */
+
+    server.get('/:id',
+        { preHandler: [checkSessionIdExist] },
+        async (req, res) => {
+
+            const getTransactionParamsSchema = z.object({
+                id: z.string().uuid(),
+            })
+
+            const { id } = getTransactionParamsSchema.parse(req.params)
+
+            //Puxar transaçoes unicas || Valida se o cookie esta presente
+            const { sessionID } = req.cookies
+
+            const transaction = await db('transactions')
+                .where({
+                    'session_id': sessionID,
+                    'id': id,
+                })
+                .first()
+
+            return { transaction }
+
+        })
+
+    /* GET DE LISTAGEM DE SOMATORIO FINAL */
+
+    server.get('/summary',
+        { preHandler: [checkSessionIdExist] },
+        async (req) => {
+
+            //Puxar transaçoes unicas || Valida se o cookie esta presente
+            const { sessionID } = req.cookies
+
+            //Realiza a soma dos valores e com (as:'amount') muda o nome da coluna 
+            const summary = await db('transactions')
+            .where('session_id',sessionID)
+            .sum('amount', { as: 'amount' })
+            return { summary }
+        })
 
 
 }
