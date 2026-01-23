4b9fd1c4a47639ab5b7b62ab593b9f4ecf6eabf4
Lucas Rodrigues <lucas.bartllb@gmail.com>
2026-01-23 14:51:55 -0300

    categorias(describe) em vitest

--- src/test/example.test.ts
+++ src/test/example.test.ts
@@ -1,32 +1,35 @@
-import {expect, test, beforeAll, afterAll} from 'vitest'
+import { expect, test, beforeAll, afterAll, describe } from 'vitest'
 import request from 'supertest'
-import {server} from '../app.js'
+import { server } from '../app.js'
 
 
-//Utilizando esse beforeAll para aguardar a utilização dos plugins já existentes 
-beforeAll(async()=>{
+describe('transactions rotes', () => {
+    //Utilizando esse beforeAll para aguardar a utilização dos plugins já existentes 
+    beforeAll(async () => {
 
-    await server.ready()
+        await server.ready()
 
-})
-
-//Após a execuçào dos testes esse metodo finaliza a aplicação 
-afterAll(async()=>{
-
-    await server.close()
-    
-})
-
-test('O usuário consegue criar uma nova transação', async()=>{
-
-    //Fazer a chamada Http para criar nova transação p criar nova transação
-    await request(server.server)
-    .post('/transactions')
-    .send({
-        title:'test transactions',
-        amount:12,
-        type: 'debit'
     })
-    .expect(201)
-   
+
+    //Após a execuçào dos testes esse metodo finaliza a aplicação 
+    afterAll(async () => {
+
+        await server.close()
+
+    })
+
+    test('O usuário consegue criar uma nova transação', async () => {
+
+        //Fazer a chamada Http para criar nova transação p criar nova transação
+        await request(server.server)
+            .post('/transactions')
+            .send({
+                title: 'test transactions',
+                amount: 12,
+                type: 'debit'
+            })
+            .expect(201)
+
+    })
 })
+
